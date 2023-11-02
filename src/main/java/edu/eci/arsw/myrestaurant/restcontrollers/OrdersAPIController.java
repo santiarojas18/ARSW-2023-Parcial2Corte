e8593a6507e7b516944ea70408266d71dff7838c
unknown <santiago.arevalo-r@labinfo.is.escuelaing.edu.co>
2023-11-02 09:51:00 -0500

    Finalizado sin bono

--- src/main/java/edu/eci/arsw/myrestaurant/restcontrollers/OrdersAPIController.java
+++ src/main/java/edu/eci/arsw/myrestaurant/restcontrollers/OrdersAPIController.java
@@ -16,25 +16,59 @@
  */
 package edu.eci.arsw.myrestaurant.restcontrollers;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import edu.eci.arsw.myrestaurant.model.Order;
 import edu.eci.arsw.myrestaurant.model.ProductType;
 import edu.eci.arsw.myrestaurant.model.RestaurantProduct;
+import edu.eci.arsw.myrestaurant.services.OrderServicesException;
 import edu.eci.arsw.myrestaurant.services.RestaurantOrderServicesStub;
-import java.util.Hashtable;
-import java.util.Map;
+
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 /**
  *
  * @author hcadavid
  */
+@RestController
+@RequestMapping(value = "/orders")
 public class OrdersAPIController {
+    @Autowired
+    RestaurantOrderServicesStub ross;
 
-    
+    @RequestMapping(method = RequestMethod.GET)
+    @ExceptionHandler(OrderServicesException.class)
+    public ResponseEntity<?> handlerGetRequest() {
+        Set<Integer> orders = ross.getTablesWithOrders();
+        Order currentOrder;
+        Gson myGson = new Gson();
+        JsonArray finalArray = new JsonArray();
+        for (Integer order : orders) {
+            currentOrder = ross.getTableOrder(order);
+            try {
+                JsonObject myJson = new JsonObject();
+                Integer bill = ross.calculateTableBill(order);
+                myJson.addProperty("table", order);
+                JsonArray orderArray = new JsonArray();
+                Map<String, Integer> orderMap = currentOrder.getOrderAmountsMap();
+                for (String food : orderMap.keySet()) {
+                    JsonObject foodObject = new JsonObject();
+                    foodObject.addProperty(food, orderMap.get(food));
+                    orderArray.add(foodObject);
+                }
+                myJson.add("order", orderArray);
+                myJson.addProperty("total", bill);
+                finalArray.add(myJson);
+            } catch (OrderServicesException ex) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        }
+        return new ResponseEntity<>(myGson.toJson(finalArray), HttpStatus.ACCEPTED);
+    }
 }
--- src/main/java/edu/eci/arsw/myrestaurant/services/RestaurantOrderServicesStub.java
+++ src/main/java/edu/eci/arsw/myrestaurant/services/RestaurantOrderServicesStub.java
@@ -1,21 +1,25 @@
 package edu.eci.arsw.myrestaurant.services;
 
 
+import edu.eci.arsw.myrestaurant.beans.impl.BasicBillCalculator;
 import edu.eci.arsw.myrestaurant.model.Order;
 import edu.eci.arsw.myrestaurant.model.RestaurantProduct;
 import edu.eci.arsw.myrestaurant.beans.BillCalculator;
 import edu.eci.arsw.myrestaurant.model.ProductType;
+import org.springframework.stereotype.Service;
+
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
-
+@Service
 public class RestaurantOrderServicesStub implements RestaurantOrderServices {
 
     
     BillCalculator calc = null;
 
     public RestaurantOrderServicesStub() {
+        calc = new BasicBillCalculator();
     }
 
     public void setBillCalculator(BillCalculator calc) {
